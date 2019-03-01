828725d6a30e2811bb54a3712ccd8fbf7200474b
Mateja Petrovic <mateja176@gmail.com>
2019-03-01 23:10:03 +0100

    Uses date pipe to display date depending on locale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,40 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [TranslatePipe],
+  providers: [TranslatePipe, DatePipe],
 })
 export class AppComponent implements OnInit {
   title: string;
 
+  now = new Date();
+
+  date = new BehaviorSubject<string>(new Date().toString());
+
   constructor(
     private translate: TranslateService,
     private translatePipe: TranslatePipe,
+    private datePipe: DatePipe,
   ) {
     translate.setDefaultLang('en');
   }
 
   ngOnInit() {
     this.title = this.translatePipe.transform('title');
+
+    this.date.next(this.datePipe.transform(this.date.value));
   }
 
   useLanguage(language: string) {
     this.translate.use(language);
+  }
+
+  useLocale(locale: string) {
+    this.date.next(this.datePipe.transform(new Date(), 'longDate', '', locale));
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,7 @@
+import { registerLocaleData } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import localeAr from '@angular/common/locales/ar';
+import localeDe from '@angular/common/locales/de';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 // import ngx-translate and the http loader
@@ -7,33 +10,31 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+registerLocaleData(localeDe);
+registerLocaleData(localeAr);
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
 
-        // configure the imports
-        HttpClientModule,
-        TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [HttpClient]
-            }
-        })
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+    // configure the imports
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-
-export class AppModule {
-}
+export class AppModule {}
 
 // required for AOT compilation
 export function HttpLoaderFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http);
 }
